0f9647fdc00bda4b6005d086649b949798be7dac
Maheboob-baig15 <maheboob.baiig@gmail.com>
2025-05-12 06:44:31 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { Link } from "react-router-dom";
 import Pokedex from "./Pokedex/Pokedex"
+import './App.css'
+import CustomRoutes from "./routes/CustomRutes";
 
 
 function App() {
  
 
   return (
-    <>
-
-       <Pokedex/>
-    </>
+    <div className="outer-pokedex">
+      <h1 id="pokedex-heading">
+        <Link to="/">Pokedex</Link> 
+      </h1>
+      <CustomRoutes />
+    </div>
   );
 }
 
